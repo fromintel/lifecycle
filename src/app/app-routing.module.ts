e822740d1f3f6125439309fc52a61564cb643db0
Anton Zolotuhin <anton.zolotukhin@nixsolutions.com>
2019-12-24 18:49:22 +0200

    init lifecycle hooks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { OverviewComponent } from './overview/overview.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'overview', component: OverviewComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: '',
+    redirectTo: '/overview',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { OverviewComponent } from './overview/overview.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    OverviewComponent,
+    AboutPageComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
